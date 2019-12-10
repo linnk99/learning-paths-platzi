508f3510a456afa7872438941d59c14b47d69479
Hector Vega <linnk99@gmail.com>
2019-12-09 23:33:58 -0500

    Defining first tuples of data

--- logic.py
+++ logic.py
@@ -1,43 +1,74 @@
+areas_de_conocimiento = (
+    'Desarrollo e Ingenieria',
+    'Diseño y UX',
+    'Marketing Digital',
+    'Negocios y Emprendimiento',
+    'Producción Audiovisual',
+    'Crecimiento Profesional',    
+)
+
+rutas_desarrollo = (
+    'Fundamentos de Programación',
+    'Desarrollo Web Frontend',
+    'Desarrollo Web Backend',
+    'Bases de Datos',
+    'Desarrollo Móvil',
+    'Big Data y Data Science',
+    'Inteligencia Artificial',
+    'Internet of Things',
+    'Seguridad Informática',
+    'Cloud Technologies',
+    'Desarrollo de Videojuegos'
+)
+
+rutas_diseno = (
+    
+)
+
+rutas_marketing = (
+    
+)
+
 class LearningPath():
     
-    def __init__(self, categoria, especialidad, nivel):
+    def __init__(self, categoria, especialidad, nivel, nombre):
         self.categoria = categoria
         self.especialidad = especialidad
         self.nivel = nivel
+        self.nombre = nombre
 
 class ListaRutas():
     
     def __init__():
         self.rutas = []
-    
 
-def command(option):
-    if option == 'd':
-        
-        print('Elegiste Desarrollo e Ingenieria')
+    def area_de_conocimiento(option):
+        if option == 'd':
+            rutas[0] = 'Desarrollo e Ingenieria'
+            print('Elegiste Desarrollo e Ingenieria')
 
-    elif option == 'di':
-        
-        print('Elegiste Diseño y UX')
-        
-    elif option == 'm':
-        
-        print('Elegiste Marketing Digital')
-        
-    elif option == 'n':
-        
-        print('Elegiste Negocios y Emprendimiento')
-        
-    elif option == 'p':
-        
-        print('Elegiste Producción Audiovisual')
+        elif option == 'di':
+            rutas[0] = 'Diseño y UX'
+            print('Elegiste Diseño y UX')
+            
+        elif option == 'm':
+            rutas[0] = 'Marketing Digital'
+            print('Elegiste Marketing Digital')
+            
+        elif option == 'n':
+            rutas[0] = 'Negocios y Emprendimiento'
+            print('Elegiste Negocios y Emprendimiento')
+            
+        elif option == 'p':
+            rutas[0] = 'Producción Audiovisual'
+            print('Elegiste Producción Audiovisual')
 
-    elif option == 'c':
-        
-        print('Elegiste Crecimiento Profesional')
-        
-    else:
-        print('Opción no válida, intenta de nuevo por favor.')
+        elif option == 'c':
+            rutas[0] = 'Crecimiento Profesional'
+            print('Elegiste Crecimiento Profesional')
+            
+        else:
+            print('Opción no válida, intenta de nuevo por favor.')
         
 
 def run():
@@ -53,7 +84,7 @@ def run():
             ''')    
     
     while True:
-        option = str(input('''Elige una opción: 
+        option = str(input('''Elige un área de conocimiento: 
                            
                            [D]esarollo e Ingeniería
                            [Di]seño y UX
@@ -64,7 +95,7 @@ def run():
                            
                            ''').lower())
         
-        command(option)
+        ListaRutas.area_de_conocimiento(option)
 
 if __name__ == "__main__":
     run()
